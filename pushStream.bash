6ab6bb9cd88b8649a8b9d752bd9d2a699f3b60d0
Xiahl123 <68577036+Xiahl123@users.noreply.github.com>
2024-09-10 16:38:54 +0800

    alter

--- pushStream.bash
+++ pushStream.bash
@@ -1,9 +1,9 @@
 #!/bin/bash
-     INRES="1366x768" # input resolution
-     OUTRES="1366x768" # output resolution
-     FPS="30" # target FPS
-     GOP="60" # i-frame interval, should be double of FPS,
-     GOPMIN="30" # min i-frame interval, should be equal to fps,
+     INRES="6480x3840" # input resolution
+     OUTRES="3240x1920" # output resolution
+     FPS="20" # target FPS
+     GOP="40" # i-frame interval, should be double of FPS,
+     GOPMIN="20" # min i-frame interval, should be equal to fps,
      THREADS="2" # max 6
      CBR="1000k" # constant bitrate (should be between 1000k - 3000k)
      QUALITY="ultrafast"  # one of the many FFMPEG preset
@@ -12,7 +12,7 @@
      STREAM_KEY="$1" # paste the stream key after calling stream_now
      SERVER="live-mia" # twitch server in miami Florida, see https://stream.twitch.tv/ingests/ for list
 
-     ffmpeg -f x11grab -s "$INRES" -r "$FPS" -i :0.0 -f pulse -i 0 -f flv -ac 2 -ar $AUDIO_RATE \
-       -vcodec libx264 -g $GOP -keyint_min $GOPMIN -b:v $CBR -minrate $CBR -maxrate $CBR -pix_fmt yuv420p\
-       -s $OUTRES -preset $QUALITY -tune film -acodec aac -threads $THREADS -strict normal \
-       -bufsize $CBR -probesize $PROBESZ "rtmp://$SERVER.twitch.tv/app/$STREAM_KEY"
+     ffmpeg -video_size $INRES -r $FPS -f x11grab -i :1.0+0,0 -f flv -vcodec h264_nvenc \
+     -g $GOP -keyint_min $GOPMIN -b:v $CBR -minrate $CBR -maxrate $CBR -pix_fmt yuv420p \
+     -s $OUTRES -preset $QUALITY -bufsize $CBR -probesize $PROBESZ \
+     -flvflags no_duration_filesize rtmp://192.168.1.2:1935/live/testv
